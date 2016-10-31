adb32136d7c11c1b0aa2bc89d1fe056d3770396e
aniakubik <ania.kubik@gmail.com>
2016-10-31 12:40:43 +0100

    code for submission
    implementations, costs and helpers ok, build_polynomial and run (earlier run log reg) to check

---
+++ Submission/build_polynomial.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""implement a polynomial basis function."""
+
+import numpy as np
+import sys, os
+sys.path.append(os.path.join(sys.path[0],'Provided files','scripts'))
+from helpers import standardize
+
+def build_degree2_poly (x):
+    """polynomial basis functions for input data x, for j=1 up to j=degree."""
+
+    N = x.shape[0]
+
+    x_st = standardize(x)[0]
+    # x_st = np.hstack((np.ones((x.shape[0],1)), x))
+
+    M = x_st.shape[1]
+
+    phi = np.empty([N,int(M*(M+1)/2)])
+
+    a = (M+1)/2
+
+    for i in range(int(np.floor(a))):
+        phi[:, M*i:M*(i+1)] = x_st*np.roll(x_st,i,axis=1)
+
+    if M % 2 == 0:
+        phi[:,np.floor(a)*M:] = x_st[:,0:int(M/2)]*x_st[:,int(M/2):]
+
+    # print(np.linalg.matrix_rank(phi))
+
+    return phi
+
+def build_degrees_nm(x,n,m):
+    N = x.shape[0]
+    M = x.shape[1]
+
+    x_st = standardize(x)[0]
+    x_st = x_st[:,1:]
+
+    phi = np.zeros((N,M*(m-n+1)))
+
+    for i in range(n,m):
+        j = i-n
+        phi[:,M*j:M*(j+1)] = x_st**i
+
+    return phi
---
+++ Submission/costs.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""a function used to compute the loss."""
+
+import numpy as np
+from helpers import *
+
+
+def calculate_mse(e):
+    """Calculate the mse for vector e."""
+    return 1/2*np.mean(e**2)
+
+
+def calculate_mae(e):
+    """Calculate the mae for vector e."""
+    return np.mean(np.abs(e))
+
+
+def compute_loss(y, tx, w):
+    """Calculate the loss.
+
+    You can calculate the loss using mse or mae.
+    """
+    e = y - tx.dot(w)
+    return calculate_mse(e)
+    # return calculate_mae(e)
+
+
+def error_percent(y, tx, w):
+
+    y_pred = predict_labels(w,tx)
+
+    return np.mean(y_pred != y)
